1a18e6102ea79f800c3b4c374be82c5e0998ed01
johnpappin <johnpappin@1b5efb4e-a12c-0410-9749-51f16257a6fc>
2007-03-29 01:38:56 +0000

    checking in the spike code

---
+++ src/main/java/com/googlecode/gwt/crypto/TripleDesCipher.java
@@ -0,0 +1,66 @@
+package com.googlecode.gwt.crypto;
+
+import com.googlecode.gwt.crypto.bouncycastle.DataLengthException;
+import com.googlecode.gwt.crypto.bouncycastle.InvalidCipherTextException;
+import com.googlecode.gwt.crypto.bouncycastle.engines.DESedeEngine;
+import com.googlecode.gwt.crypto.bouncycastle.modes.CBCBlockCipher;
+import com.googlecode.gwt.crypto.bouncycastle.paddings.PaddedBufferedBlockCipher;
+import com.googlecode.gwt.crypto.bouncycastle.params.KeyParameter;
+import com.googlecode.gwt.crypto.bouncycastle.util.encoders.Hex;
+import com.googlecode.gwt.crypto.util.Str;
+
+
+//import org.bouncycastle.crypto.DataLengthException;
+//import org.bouncycastle.crypto.InvalidCipherTextException;
+//import org.bouncycastle.crypto.engines.DESedeEngine;
+//import org.bouncycastle.crypto.modes.CBCBlockCipher;
+//import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+//import org.bouncycastle.crypto.params.KeyParameter;
+//import org.bouncycastle.util.encoders.Hex;
+
+public class TripleDesCipher {
+	private PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(
+			new CBCBlockCipher(new DESedeEngine()));
+	private byte[] key = null;
+
+	public TripleDesCipher() {
+		super();
+	}
+
+	public byte[] getKey() {
+		return key;
+	}
+
+	public void setKey(byte[] key) {
+		this.key = key;
+	}
+
+	public String encrypt(String plainText) throws DataLengthException,
+			IllegalStateException, InvalidCipherTextException {
+		cipher.reset();
+		cipher.init(true, new KeyParameter(key));
+
+		// byte[] input = plainText.getBytes();
+		byte[] input = Str.toBytes(plainText.toCharArray());
+		byte[] output = new byte[cipher.getOutputSize(input.length)];
+
+		int length = cipher.processBytes(input, 0, input.length, output, 0);
+		int remaining = cipher.doFinal(output, length);
+		byte[] result = Hex.encode(output, 0, output.length);
+		return new String(Str.toChars(result));
+	}
+
+	public String decrypt(String cipherText) throws DataLengthException,
+			IllegalStateException, InvalidCipherTextException {
+		cipher.reset();
+		cipher.init(false, new KeyParameter(key));
+
+		byte[] input = Hex.decode(Str.toBytes(cipherText.toCharArray()));
+		byte[] output = new byte[cipher.getOutputSize(input.length)];
+
+		int length = cipher.processBytes(input, 0, input.length, output, 0);
+		int remaining = cipher.doFinal(output, length);
+		return new String(Str.toChars(output), 0, length + remaining);
+	}
+
+}
