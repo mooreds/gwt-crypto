da87907033e52830aaf8cfcd27f7d53bac4ff6b5
mooreds <mooreds@1b5efb4e-a12c-0410-9749-51f16257a6fc>
2009-10-04 03:51:35 +0000

    added additional tests, code fix for issue #3

--- src/main/java/com/googlecode/gwt/crypto/util/Str.java
+++ src/main/java/com/googlecode/gwt/crypto/util/Str.java
@@ -1,19 +1,29 @@
 package com.googlecode.gwt.crypto.util;
 
 public class Str {
-	public static byte[] toBytes(char[] inputChars) {
-		byte[] out = new byte[inputChars.length];
-		for (int i = 0; i < inputChars.length; i++) {
-			out[i] = (byte) inputChars[i];
-		}
-		return out;
-	}
 
-	public static char[] toChars(byte[] in) {
-		char[] out = new char[in.length];
-		for (int i = 0; i < in.length; i++) {
-			out[i] = (char) in[i];
-		}
-		return out;
+   private static int[]delta;
+
+   public static byte[] toBytes(char[] inputChars) {
+        byte[] out = new byte[inputChars.length];
+        delta = new int[inputChars.length];
+	for (int i = 0; i < inputChars.length; i++) {
+	    out[i] = (byte) inputChars[i];
+            delta[i] = (int) inputChars[i] - out[i];
 	}
+	return out;
+    }
+
+    public static char[] toChars(byte[] in) {
+	char[] out = new char[in.length];
+	for (int i = 0; i < in.length; i++) {
+            if (i < delta.length) {
+                out[i] = (char) (in[i] + delta[i]);
+            } else {
+                out[i] = (char) in[i];
+            }
+        }
+        return out;
+    }
+
 }
--- src/test/java/com/googlecode/gwt/crypto/client/TripleDesCipherTest.java
+++ src/test/java/com/googlecode/gwt/crypto/client/TripleDesCipherTest.java
@@ -5,9 +5,48 @@ import junit.framework.TestCase;
 public class TripleDesCipherTest extends TestCase {
 	private final static byte[] key = new byte[]{(byte)4,(byte)8,(byte)3,(byte)80,(byte)12,(byte)-9,(byte)-5,(byte)101, (byte)15,(byte)-8,(byte)3,(byte)0,(byte)90,(byte)-9,(byte)55,(byte)-41, (byte)-9,(byte)90,(byte)3,(byte)100,(byte)-40,(byte)79,(byte)5,(byte)102};
 
-	private final static String toEncrypt = "encrypt This";
 
 	public void testEncryptDecrypt() throws Exception {
+	 	String toEncrypt = "encrypt This";
+		TripleDesCipher cipher = new TripleDesCipher();
+		cipher.setKey(key);
+		String encrypted = cipher.encrypt(toEncrypt);
+		String decrypted = cipher.decrypt(encrypted);
+		assertEquals(toEncrypt, decrypted);
+	}
+
+
+	public void testUnicodeEncryptDecrypt() throws Exception {
+	 	String toEncrypt = "\u00D6 \u00C4 (German)\n";
+		TripleDesCipher cipher = new TripleDesCipher();
+		cipher.setKey(key);
+		String encrypted = cipher.encrypt(toEncrypt);
+		String decrypted = cipher.decrypt(encrypted);
+		assertEquals(toEncrypt, decrypted);
+
+		// chinese sample from here: http://www.chinesecomputing.com/programming/java.html
+ 		String chinesesample = "\u4e00";
+		encrypted = cipher.encrypt(chinesesample);
+		decrypted = cipher.decrypt(encrypted);
+		assertEquals(chinesesample, decrypted);
+
+	}
+
+	public void testMultipleEncryptDecrypt() throws Exception {
+	 	String toEncrypt = "encrypt this";
+	 	String toEncrypt2 = "now, encrypt this";
+		TripleDesCipher cipher = new TripleDesCipher();
+		cipher.setKey(key);
+		String encrypted = cipher.encrypt(toEncrypt);
+		String decrypted = cipher.decrypt(encrypted);
+		assertEquals(toEncrypt, decrypted);
+		encrypted = cipher.encrypt(toEncrypt2);
+		decrypted = cipher.decrypt(encrypted);
+		assertEquals(toEncrypt2, decrypted);
+	}
+
+	public void testLargeStringEncryptDecrypt() throws Exception {
+	 	String toEncrypt = " encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string encrypt this large large string ";
 		TripleDesCipher cipher = new TripleDesCipher();
 		cipher.setKey(key);
 		String encrypted = cipher.encrypt(toEncrypt);
